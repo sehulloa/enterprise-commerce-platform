e69cf13406abdb11336a9c4f91e62db7ffef94c3
Salvador Ulloa <sehulloa@gmail.com>
2026-05-07 01:06:32 -0600

    infra: add ecs task definition terraform model

---
+++ infrastructure/terraform/aws/ecs_task.tf
@@ -0,0 +1,39 @@
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${local.name_prefix}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = "512"
+  memory = "1024"
+
+  execution_role_arn = "arn:aws:iam::123456789012:role/ecsTaskExecutionRole" # placeholder
+
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = "${aws_ecr_repository.app.repository_url}:latest"
+
+      portMappings = [
+        {
+          containerPort = 8080
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name  = "SPRING_PROFILES_ACTIVE"
+          value = "prod"
+        },
+        {
+          name  = "SERVER_PORT"
+          value = "8080"
+        }
+      ]
+
+      essential = true
+    }
+  ])
+
+  tags = local.common_tags
+}
--- infrastructure/terraform/aws/outputs.tf
+++ infrastructure/terraform/aws/outputs.tf
@@ -26,3 +26,7 @@ output "ecr_repository_url" {
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.main.name
 }
+
+output "ecs_task_definition_family" {
+  value = aws_ecs_task_definition.app.family
+}
