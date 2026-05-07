1e63855e22241587a8befb85acd0b1ac68d7fe4c
Salvador Ulloa <sehulloa@gmail.com>
2026-05-07 00:50:00 -0600

    infra: add ecs cluster terraform definition

--- infrastructure/terraform/aws/outputs.tf
+++ infrastructure/terraform/aws/outputs.tf
@@ -22,3 +22,7 @@ output "ecr_repository_url" {
   description = "ECR repository URL for the application image."
   value       = aws_ecr_repository.app.repository_url
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
